29f8707203b62da561e840d8c332b6bf54354fa4
Matthew Tran <matthewtran1215@gmail.com>
2025-12-28 16:37:53 -0800

    made dark mode manual

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,12 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen bg-gray-100">
         {/* Top Bar */}
         <Topbar />
 
         {/* Page Content */}
-        <main className="flex-1 overflow-y-auto bg-red-500">
+        <main className="flex-1 overflow-y-auto bg-zinc-50">
           {children}
         </main>
       </body>
---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+};
+
+export default config;
